8c53d229da559f0108fcbfd7ffad96e60208c6d9
Alex Poms <alexpoms@gmail.com>
2013-11-29 16:23:20 -0800

    Fix style issues and initializers. Add depth testing for multiple objects.

--- src/main.py
+++ src/main.py
@@ -2,42 +2,73 @@ from Tkinter import Canvas, PhotoImage, Tk, mainloop
 from scene_objects import Light, Camera, Ray, Sphere, Plane
 import numpy as np
 import matrix
+import math
+import sys
 
-HEIGHT = 800
-WIDTH = 800
+
+HEIGHT = 400
+WIDTH = 400
 
 world_objects = set()
 light_objects = set()
 
 
-def compute_camera_ray(width, height, camera, x, y):
-    normalized_x = ((1.0 * x / width) - 0.5) * 4
-    normalized_y = (0.5 - (1.0 * y / height)) * 4
+def compute_camera_ray(width, height, camera, view_width, x, y):
+    normalized_x = ((1.0 * x / width) - 0.5) * view_width
+    normalized_y = (0.5 - (1.0 * y / height)) * view_width
     ray_direction = matrix.normalize(normalized_x * camera.right +
                                      normalized_y * camera.up +
                                      camera.direction)
-    ray = Ray()
-    ray.origin = camera.position
-    ray.vector = ray_direction
+    ray = Ray(origin=camera.position, vector=ray_direction)
     return ray
 
 
 def compute_light(light, intersection, normal):
     to_light = matrix.normalize(light.position - intersection)
     shade = np.dot(to_light, normal)
-    if shade > 0:
-        return shade
-    return 0
+    return max(shade, 0)
+
+
+def point_on_line(ray, distance):
+    return ray.origin + distance * ray.vector
 
 
 def trace_ray(world, lights, ray):
+    depth = float('inf')
+    color = np.array([0, 0, 0])
     for obj in world:
-        point, normal = obj.get_intersection(ray)
-        if point is not None:
+        distance, normal = obj.get_intersection(ray)
+        if distance is not None and distance < depth:
+            depth = distance
+            point = point_on_line(ray, distance)
             for light in lights:
                 shade = compute_light(light, point, normal)
-                return obj.diffuse * (shade * 0.8 + 0.2)
-    return np.array([0, 0, 0])
+                color = obj.diffuse * (shade * 0.8 + 0.2)
+    return color
+
+
+def init_world(world):
+    red = Sphere(center=np.array([0.0, 4.0, 6.0]),
+                 radius=4.0,
+                 diffuse=np.array([255, 0, 0]))
+    world_objects.add(red)
+
+    blue = Sphere(center=np.array([-5.0, 3.0, 0.0]),
+                  radius=3.0)
+    world_objects.add(blue)
+
+    green = Sphere(center=np.array([5.0, 2.0, 0.0]),
+                   radius=2.0,
+                   diffuse=np.array([0, 255, 0]))
+    world_objects.add(green)
+
+    plane = Plane(normal=np.array([0, 1, 0]), distance=0.0)
+    world_objects.add(plane)
+
+
+def init_light(lights):
+    light = Light(position=np.array([5.0, 5.0, -3.0]))
+    lights.add(light)
 
 
 def main():
@@ -49,38 +80,32 @@ def main():
     w.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
 
     cam = Camera()
-    cam.look_at(np.array([0, 5, -10]),
-                np.array([0, 0, 0]),
+    cam.look_at(np.array([0, 10, -8]),
+                np.array([0, 0, 3.0]),
                 np.array([0, 1.0, 0]))
 
-    sphere = Sphere()
-    sphere.radius = 4
-    #world_objects.add(sphere)
-    red = Sphere()
-    red.center = np.array([0.0, -1.0, 0.0])
-    red.radius = 4.0
-    world_objects.add(red)
-
-    plane = Plane()
-    plane.normal = np.array([0, 1, 0])
-    plane.distance = 4
-    world_objects.add(plane)
-
-    light = Light()
-    light.position = np.array([5.0, 5.0, -3.0])
-    light_objects.add(light)
-
+    init_world(world_objects)
+    init_light(light_objects)
     # Generate rays for each pixel and determine color
+    progress_interval = HEIGHT*WIDTH / 10
+    progress_tick = 0
+    print 'Progress (10 ticks): [ -',
+    sys.stdout.flush()
     for y in xrange(HEIGHT):
         for x in xrange(WIDTH):
-            ray = compute_camera_ray(WIDTH, HEIGHT, cam, x, y)
+            progress_tick += 1
+            if progress_tick > progress_interval:
+                progress_tick = 0
+                print ' -',
+                sys.stdout.flush()
+            ray = compute_camera_ray(WIDTH, HEIGHT, cam, 3, x, y)
             color = trace_ray(world_objects, light_objects, ray)
             # TKinter requires a hex string as color input for photo image
             img.put('#%02X%02X%02X' % tuple(color), (x, y))
+    print ' - ]'
     mainloop()
 
 if __name__ == "__main__":
-    pass
-    ##main()
+    main()
 
     
--- src/scene_objects.py
+++ src/scene_objects.py
@@ -2,42 +2,78 @@ import numpy as np
 import matrix
 import math
 
+np_array_zero = np.array([0.0, 0.0, 0.0])
+
 
 class Light(object):
-    def __init__(self):
-        self.position = np.array([0, 0, 0])
-        self.intensity = 10
+    def __init__(self,
+                 position=None,
+                 intensity=None):
+        self.position = position
+        self.intensity = intensity
+        if position is None:
+            self.position = np_array_zero
+        if intensity is None:
+            self.intensity = 10
 
 
 class Camera(object):
-    def __init__(self):
-        self.position = np.array([0, 0, 0])
-        self.direction = np.array([0, 0, 0])
-        self.right = np.array([0, 0, 0])
-        self.up = np.array([0, 0, 0])
+    def __init__(self,
+                 position=None,
+                 direction=None,
+                 up=None):
+        self.position = position
+        self.direction = direction
+        self.up = up
+        if position is None:
+            self.position = np.array([0, 0, 0])
+        if direction is None:
+            self.direction = np.array([0, 0, 1])
+        if up is None:
+            self.up = np.array([0, 1, 0])
+        self.__compute_right_up()
 
     def look_at(self, eye, center, up):
         self.position = eye
         self.direction = matrix.normalize(center - eye)
         self.up = matrix.normalize(up)
+        self.__compute_right_up()
+
+    def __compute_right_up(self):
         self.right = matrix.normalize(np.cross(self.direction, self.up))
         self.up = np.cross(self.right, self.direction)
 
 
 class Ray(object):
-    def __init__(self):
-        self.origin = np.array([0.0, 0.0, 0.0])
-        self.vector = np.array([0.0, 0.0, 0.0])
+    def __init__(self,
+                 origin=None,
+                 vector=None):
+        self.origin = origin
+        self.vector = vector
+        if origin is None:
+            self.origin = np.array([0, 0, 0])
+        if vector is None:
+            self.vector = np.array([0, 0, 1])
 
     def __str__(self):
-        return "origin: " + str(self.origin) + ", vector: " + str(self.vector)
+        return "origin: %s, vector: %s".format(str(self.origin),
+                                               str(self.vector))
 
 
 class Plane(object):
-    def __init__(self):
-        self.normal = np.array([0, 0, 0])
-        self.distance = 0
-        self.diffuse = np.array([255, 255, 255])
+    def __init__(self,
+                 normal=None,
+                 distance=None,
+                 diffuse=None):
+        self.normal = normal
+        self.distance = distance
+        self.diffuse = diffuse
+        if normal is None:
+            self.normal = np.array([0, 0, -1])
+        if distance is None:
+            self.distance = 5
+        if diffuse is None:
+            self.diffuse = np.array([255, 255, 255])
 
     def get_intersection(self, ray):
         numerator = -self.distance - np.dot(ray.origin, self.normal)
@@ -45,26 +81,36 @@ class Plane(object):
         if denominator != 0:
             t = numerator / denominator
             if t > 0:
-                return ray.origin + t * ray.vector, self.normal
+                return t, self.normal
         return None, None
 
 
 class Sphere(object):
-    def __init__(self):
-        self.center = np.array([0.0, 0.0, 0.0])
-        self.radius = 0
-        self.diffuse = np.array([0, 0, 255.0])
+    def __init__(self,
+                 center=None,
+                 radius=None,
+                 diffuse=None):
+        self.center = center
+        self.radius = radius
+        self.diffuse = diffuse
+        if center is None:
+            self.center = np.array([0.0, 0.0, 0.0])
+        if radius is None:
+            self.radius = 0
+        if diffuse is None:
+            self.diffuse = np.array([0, 0, 255.0])
 
     def get_intersection(self, ray):
         c = (self.center - ray.origin)
-        c_distance_sqred = (c ** 2).sum()
+        c_distance_sqred = (c**2).sum()
         proj_on_ray = np.dot(c, ray.vector)
-        d = (self.radius ** 2 - (c_distance_sqred - proj_on_ray ** 2))
-        if (d >= 0):
+        d = self.radius ** 2 - (c_distance_sqred - proj_on_ray ** 2)
+        if d >= 0:
             d = math.sqrt(d)
             v = proj_on_ray
-            intersection_point = ray.origin + (v - d) * ray.vector
+            distance = v - d
+            intersection_point = ray.origin + distance * ray.vector
             # find normal
             normal = matrix.normalize(intersection_point - self.center)
-            return intersection_point, normal
+            return distance, normal
         return None, None
